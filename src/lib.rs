fc012753e8e14cc662e8d1b8fba2213fc0203ef0
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-01-04 00:38:22 +0100

    Add documentation from main module to `dedent` in `src/lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
 extern crate proc_macro_hack;
 
 #[proc_macro_hack::proc_macro_hack]
+/// Removes common leading whitespace from each line.
+///
+/// This macro will look at each non-empty line and determine the maximum
+/// amount of whitespace that can be removed from all lines, and create a
+/// new string literal in place of the old one.
+///
+/// ```rust
+/// # extern crate textwrap_macros;
+/// # use textwrap_macros::dedent;
+///
+/// const X: &str = dedent!("
+///   1st line
+///     2nd line
+///   3rd line
+/// ");
+///
+/// assert_eq!(X, "
+/// 1st line
+///   2nd line
+/// 3rd line
+/// ");
+/// ```
 pub use textwrap_macros_impl::dedent;
 
 #[proc_macro_hack::proc_macro_hack]
+/// Add prefix to each non-empty line.
 pub use textwrap_macros_impl::indent;
